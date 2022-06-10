143dd3f6068e0a38a19978e3704d4e17cd543e4d
toshick <toshickjazz@mac.com>
2022-06-10 11:00:54 +0900

    feat: docs

--- drumroll/nuxt.config.ts
+++ drumroll/nuxt.config.ts
@@ -3,9 +3,14 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: 'static',
+  ssr: false,
+  // rootDir: 'docs',
   css: ['@/assets/css/app.css'],
 
   generate: {
     dir: './docs',
   },
+  // router: {
+  //   base: '/docs/',
+  // },
 });
